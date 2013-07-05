d6adf676825e9501c819445f5265260a93b84e43
MrTeera <webmaster@mrteera.com>
2013-07-05 16:27:21 +0700

    add another route

--- mrteera-blog.py
+++ mrteera-blog.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! test3'
+def index():
+    return 'Index Page'
 
+@app.route('/hello')
+def hello():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
